bbb48d11e74e475c045596d5be96b893a2703580
Jrz5220 <felixlazo94@gmail.com>
2024-07-16 22:17:59 -0400

    format API response into JSON

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -5,6 +5,7 @@
 # We do not need to import these libraries into our code since they are already deployed on the Cloud IDE server, which is the IDE we are using for this project.
 
 import requests  # handles HTTP requests
+import json # formatting JSON data
 
 # function for running sentiment analysis using the Watson NLP Bert Sentiment Analysis function
 def sentiment_analyzer(text_to_analyse):
@@ -14,7 +15,14 @@ def sentiment_analyzer(text_to_analyse):
     myobj = { "raw_document": {"text": text_to_analyse} }
     # set the headers required for the API request
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
-    # send a POST request to the API with the text and headers (an object is sent back as the response with the attribute 'text' containing the response details)
-    response = requests.post(url, json = myobj, headers = headers)
+    # send a POST request to the API with the text and headers
+    response = requests.post(url, json = myobj, headers = header)
+    # the response from the API is an object with an attribute of 'text' containing the response details.
+    # 'text' returns a nested dictionary (dictionary of dictionaries) formatted as a string.
+    # format the response from the API as JSON
+    formatted_response = json.loads(response.text)
+    # extract the label and score values from the response
+    label = formatted_response["documentSentiment"]["label"]
+    score = formatted_response["documentSentiment"]["score"]
     # return the response text from the API
-    return response.text
+    return { "label": label, "score": score }
